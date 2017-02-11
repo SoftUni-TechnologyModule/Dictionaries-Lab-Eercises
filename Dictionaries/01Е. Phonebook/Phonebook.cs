6a56722289aa2efd30f00c8dc4525216172006cc
VenciSarov <venci.sarov@gmail.com>
2017-02-11 16:36:38 +0200

    Two tasks and solutions from Dictionaries Exercises was added!

---
+++ Dictionaries/01Е. Phonebook/Phonebook.cs
@@ -0,0 +1,56 @@
+﻿namespace _01Е.Phonebook
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Phonebook
+    {
+        static void Main()
+        {
+            
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+
+                if (command[0] != "END")
+                {
+                    if (command[0] == "A")
+                    {
+                        phoneBook = FillDictionary(phoneBook, command);
+                    }
+                    else
+                    {
+                        CheckingForExistingContact(phoneBook, command);
+                    }
+                    
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        static void CheckingForExistingContact(Dictionary<string, string> phoneBook, string[] command)
+        {
+            if (phoneBook.ContainsKey(command[1]))
+            {                                            
+                Console.WriteLine($"{command[1]} -> {phoneBook[command[1]]}");
+            }
+            else
+            {
+                Console.WriteLine($"Contact {command[1]} does not exist.");
+            }
+        }
+
+        static Dictionary<string, string> FillDictionary(Dictionary<string, string> phoneBook, string[] command)
+        {
+            phoneBook[command[1]] = command[2];
+
+            return phoneBook;
+        }
+    }
+}
---
+++ Dictionaries/02E. Phonebook Upgrade/PhonebookUpgrade.cs
@@ -0,0 +1,66 @@
+﻿namespace _02E.Phonebook_Upgrade
+{
+    using System;
+    using System.Collections.Generic;
+
+    class PhonebookUpgrade
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> phoneBook = new SortedDictionary<string, string>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+
+                if (command[0] != "END")
+                {
+                    if (command[0] == "A")
+                    {
+                        phoneBook = FillDictionary(phoneBook, command);
+                    }
+                    else if (command[0] == "S")
+                    {
+                        CheckingForExistingContact(phoneBook, command);
+                    }
+                    else
+                    {
+                        PrintPhoneBook(phoneBook);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        static void PrintPhoneBook(SortedDictionary<string, string> phoneBook)
+        {
+            foreach (KeyValuePair<string, string> pair in phoneBook)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+
+        static void CheckingForExistingContact(SortedDictionary<string, string> phoneBook, string[] command)
+        {
+            if (phoneBook.ContainsKey(command[1]))
+            {
+                Console.WriteLine($"{command[1]} -> {phoneBook[command[1]]}");
+            }
+            else
+            {
+                Console.WriteLine($"Contact {command[1]} does not exist.");
+            }
+        }
+
+        static SortedDictionary<string, string> FillDictionary(SortedDictionary<string, string> phoneBook, string[] command)
+        {
+            phoneBook[command[1]] = command[2];
+
+            return phoneBook;
+        }
+    }
+}
+
